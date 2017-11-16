472a4afd374276961d7bad1b27a72f756ba9644c
Ivan <igporto@esei.uvigo.es>
2017-11-16 19:41:01 +0100

    Product Selection in Process

--- app/src/main/java/com/example/ivan/minibar/Activity2.java
+++ app/src/main/java/com/example/ivan/minibar/Activity2.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ivan on 11/9/17.
@@ -16,16 +21,25 @@ public class Activity2 extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-/*
-        Button boton_volver = (Button) findViewById(R.id.button_Return);
-        boton_volver.setOnClickListener(new View.OnClickListener() {
+        //Lista de Pedidos
+        final ArrayList<String> productos;
+        final ArrayAdapter<String> adapter;
+
+        productos=new ArrayList<String>();
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,productos);
+        final ListView listaPedidos = (ListView) findViewById(R.id.listPedidos);
+        listaPedidos.setAdapter(adapter);
+
+        ImageButton btRefrescos = (ImageButton) findViewById(R.id.btRefresco);
+
+        btRefrescos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
-                //Por defecto devolve 0 no setResult()
+                productos.add("Refresco : 1.7");
+                adapter.notifyDataSetChanged();
             }
         });
-
+/*
         Button boton_ok = (Button) findViewById(R.id.button_Ok);
         boton_ok.setOnClickListener(new View.OnClickListener() {
             @Override
